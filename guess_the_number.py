3356a9f9b14387e5bb3874e7182c52b737b93b54
Gersoning <ing.gersoncp@gmail.com>
2022-03-27 15:30:07 -0500

    Adding the project

---
+++ guess_the_number.py
@@ -0,0 +1,18 @@
+import random
+
+def run():
+    aleatorio = random.randint(1,100)
+    print("")
+    numero = int(input("Digite un numero del 1 al 100: "))
+    while numero != aleatorio:
+        if numero < aleatorio:
+            print("Creo que te aun te falta")
+            print("")
+        else:
+            print("Creo que te pasaste")
+            print("")
+        numero = int(input("Digita otro numero: ")) 
+    print("Ganaste!!!")
+
+if __name__ == "__main__":
+    run()
